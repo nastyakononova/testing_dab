0996d00f339c0f15415de846bebfbb308e3607b2
nastyakononova <nastyakononovaa76@gmail.com>
2025-08-26 20:12:05 +0200

    setup file added ver 2

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="books_databricks",  # Replace with your package name
     version="0.1.0",
-    description="A short description of your project",
     author="nastyakononovaa76@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "setuptools"
+        
     ],
     python_requires=">=3.6",
 )
