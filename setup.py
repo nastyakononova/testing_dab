9ac73e730bdd81fa72f835b30273ada48dbc6291
nastyakononova <nastyakononovaa76@gmail.com>
2025-08-28 17:39:35 +0200

    setup file added ver 12

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
-import books_databricks
+
 
 setup(
     name="books_databricks",  # Replace with your package name
     version="0.1.0",
     author="nastyakononovaa76@gmail.com",
-    packages=find_packages(),
+    url='https://databricks.com',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+     entry_points={
+    'group_1': 'run=books_databricks.main:main'
+    },
     install_requires=[
-        
+        'setuptools'
     ],
     python_requires=">=3.6",
 )
